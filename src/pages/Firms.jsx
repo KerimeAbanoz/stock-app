b0492af4dfde597325665afdf39ad3baacd6b393
kerime abanoz <krmeabnz@gmail.com>
2023-02-07 18:33:01 +0300

    firm card

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,13 +1,21 @@
 import { useEffect } from "react";
 import useStockCalls from "../hooks/useStockCalls";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Alert from "@mui/material/Alert";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
 
 // import axios from "axios";
 // import { useDispatch, useSelector } from "react-redux";
 // import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const { getFirms, getSales } = useStockCalls();
-  
+  const { getFirms } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+
   // const dispatch = useDispatch()
   // const { token } = useSelector((state) => state.auth);
   // const BASE_URL = "https://11355.fullstack.clarusway.com/";
@@ -29,10 +37,26 @@ const Firms = () => {
 
   useEffect(() => {
     getFirms();
-    getSales();
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+
+      {firms?.lenght > 0 && (
+        <Grid container justifyContent="center" gap={3}>
+          {firms?.map((firm) => (
+            <Grid item>
+              <FirmCard key={firm.id} firm={firm} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
